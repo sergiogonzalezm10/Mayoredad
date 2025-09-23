e40a0929e60e2d3c1c324d109ff8bbefb66836fa
sergio.gonzalezm <sergio.gonzalezm@106-09.info2>
2025-09-23 13:56:13 +0200

    Mayoredad

---
+++ src/mayoredad/Mayoredad.java
@@ -0,0 +1,22 @@
+package mayoredad;
+
+import java.util.Scanner;
+
+public class Mayoredad {
+
+	public static void main(String[] args) {
+		int edad;
+		boolean mayor = true;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Mi edad es: ");
+		edad = sc.nextInt();
+		mayor = edad >= 18;
+		if (mayor) {
+		System.out.println("¡Eres mayor de edad!");
+		} else {
+		System.out.println("¡Eres menor de edad!");
+		
+		sc.close();
+	}
+	}
+}
