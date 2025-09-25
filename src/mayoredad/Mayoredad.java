8bddcba809fc79b07bccf033e93d99c94a9fc2fe
sergio.gonzalezm <sergio.gonzalezm@106-09.info2>
2025-09-25 13:02:16 +0200

    Mayoredad

--- src/mayoredad/Mayoredad.java
+++ src/mayoredad/Mayoredad.java
@@ -6,19 +6,16 @@ public class Mayoredad {
 
 	public static void main(String[] args) {
 		int edad;								//Variable que almacena la edad introducida posteriormente
-		boolean mayor = false;					//Variable que comprueba si la edad es mayor
+		boolean mayor;					//Variable que comprueba si la edad es mayor
 		Scanner sc = new Scanner(System.in);	//Lee lo que introduzcamos
 		System.out.println("Mi edad es: ");		//Nos indica que escribamos la edad
 		edad = sc.nextInt();					//Lee la edad introducida y la guarda en su variable
 		mayor = edad >= 18;						//Compara la edad introducida para saber si es mayor o igual a 18
-		if (mayor) {										/*Colocamos un if para si es mayor de 18 o igual poner que es mayor
-															o sino ( else ) para decir que es menor de edad*/
-		System.out.println("¡Eres mayor de edad!");
-		} else {
-		System.out.println("¡Eres menor de edad!");
+										
+		System.out.println("¿Eres mayor de edad? " + mayor);
 		
 		sc.close();			//Cerramos escáner
 	}
 	}
-}
+
 
